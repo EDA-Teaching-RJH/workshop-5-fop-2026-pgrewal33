e7942461a3a4339c631b5e299419f98e3cf89ff7
pgrewal33 <pg10@kent.ac.uk>
2026-02-03 12:36:37 +0000

    working version

--- camel.py
+++ camel.py
@@ -1,1 +1,11 @@
 # Enter your code here
+def main():
+
+    camel = input(" write a sentence in camel case: ")
+    camel_ = ''.join(['_' + char.lower() if char.isupper() else char for char in camel])
+    
+    snake = camel_.lower()
+            
+    
+    print (snake)
+main()
