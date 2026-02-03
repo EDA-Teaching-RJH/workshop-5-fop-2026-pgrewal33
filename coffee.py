1ec41492aa763bee029732018ec0b6a02a723c0b
pgrewal33 <pg10@kent.ac.uk>
2026-02-03 13:51:41 +0000

    not yet working

--- coffee.py
+++ coffee.py
@@ -1,1 +1,13 @@
 #Your code goes here
+def main():
+
+    
+    money = int(input("please pay £0.75: "))
+    p = 75
+    while money != 75:
+        ammount_due = p - money
+        
+        if ammount_due >= 0 :
+            print (ammount_due , " is due")
+            
+main()
