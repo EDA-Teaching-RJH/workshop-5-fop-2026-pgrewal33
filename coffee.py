994eb80bce7b0910747976a07c91a12619eff643
pgrewal33 <pg10@kent.ac.uk>
2026-02-03 14:29:52 +0000

    working version

--- coffee.py
+++ coffee.py
@@ -1,13 +1,23 @@
 #Your code goes here
 def main():
-
     
-    money = int(input("please pay £0.75: "))
-    p = 75
-    while money != 75:
-        ammount_due = p - money
+    total_paid = 0
+    ammount_due = 75
+
+    while total_paid < ammount_due:
+        coin = int(input("please input a coin: "))
         
-        if ammount_due >= 0 :
-            print (ammount_due , " is due")
+        if coin in [50,20,10,5]:
+           total_paid += coin
+        if total_paid < ammount_due:
+            print(f"ammount due: {ammount_due - total_paid}p")
+
+    change = total_paid - ammount_due
+
+    if change > 0:
+        print(f"change owed: {change}p")
+
+    else:
+        print("thank you for paying")
             
 main()
